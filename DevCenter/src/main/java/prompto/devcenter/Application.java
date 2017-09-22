ef76fc330004dd3d7f31551bdecd5e417b55fdad
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2017-09-23 04:04:06 +0800

    package with CodeServer

---
+++ DevCenter/src/main/java/prompto/devcenter/Application.java
@@ -0,0 +1,11 @@
+package prompto.devcenter;
+
+import prompto.codeserver.CodeServer;
+
+public class Application {
+
+	public static void main(String[] args) throws Throwable {
+		CodeServer.main(args);
+	}
+
+}
