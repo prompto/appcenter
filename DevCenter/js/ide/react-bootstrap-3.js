b4bf5c49700c316e6c35dbf0ddf67a8a681a89cf
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2018-09-29 13:29:39 +0800

    align with pec

--- DevCenter/js/ide/react-bootstrap-3.js
+++ DevCenter/js/ide/react-bootstrap-3.js
@@ -1,12 +1,25 @@
-/* a mock implementation for language resolution purpose */
-var Button = function() {
-    this.render = function() {
-        return {};
-    };
-    return this;
+/* a mock implementation for native binding resolution purpose */
+var React = {
+	Component : function() {
+		this.render = function() { return {}; };
+		return this;
+	}
 };
 
 self.ReactBootstrap = {
-    Button: Button
+    Button: React.Component,
+    Checkbox: React.Component,
+    Radio: React.Component,
+    InputGroup: React.Component,
+    FormGroup: React.Component,
+    FormControl: React.Component,
+    ControlLabel: React.Component,
+    HelpBlock: React.Component,
+    Modal: {
+    	Title: React.Component,
+    	Header: React.Component,
+    	Body: React.Component,
+    	Footer: React.Component
+    }
 };
 
