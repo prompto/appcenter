2771da528b825f3c098f6f814775da5fd352551c
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-01-24 01:32:42 +0800

    modularized structure, towards using yarn for front end

---
+++ CodeFactory/ProjectsApp/src/main/java/ProjectsApp/App.java
@@ -0,0 +1,13 @@
+package ProjectsApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
