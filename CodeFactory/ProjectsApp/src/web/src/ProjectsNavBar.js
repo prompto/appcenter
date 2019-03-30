e78b43154831eb9c4f463d82eb9e0aaff93e8b1f
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-03-30 18:22:13 +0800

    support landing on a root topic

--- CodeFactory/ProjectsApp/src/web/src/ProjectsNavBar.js
+++ CodeFactory/ProjectsApp/src/web/src/ProjectsNavBar.js
@@ -9,13 +9,13 @@ export default class ProjectsNavBar extends React.Component {
         return <Navbar inverse fluid fixedTop>
             <Navbar.Header>
                 <Navbar.Brand pullLeft>
-                    <a href="/">Prompto Development Center</a>
+                    <a href="/">Prompto Project Explorer</a>
                 </Navbar.Brand>
             </Navbar.Header>
             <Nav pullRight>
                 <NavItem href="/data/index.html" target="_blank">Data</NavItem>
-                <NavItem href="#" target="_blank">Tutorials</NavItem>
-                <NavItem href="http://www.prompto.org" target="_blank">Reference</NavItem>
+                <NavItem href="http://www.prompto.org/tutorials" target="_blank">Tutorials</NavItem>
+                <NavItem href="http://www.prompto.org/libraries" target="_blank">Reference</NavItem>
             </Nav>
             <Navbar.Form pullRight>
                 <Button type="button" id="btnNewProject" onClick={this.props.root.newProject} style={btnStyle}>New</Button>
