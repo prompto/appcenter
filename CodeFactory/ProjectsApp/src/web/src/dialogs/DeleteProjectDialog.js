54cc0267f8b9fdadff2d6ca14b3b24f03050aacc
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-04-14 19:20:19 +0800

    fix typo

--- CodeFactory/ProjectsApp/src/web/src/dialogs/DeleteProjectDialog.js
+++ CodeFactory/ProjectsApp/src/web/src/dialogs/DeleteProjectDialog.js
@@ -24,7 +24,7 @@ export default class DeleteProjectDialog extends React.Component {
                 </Modal.Header>
                 <Modal.Body>
                     { "Delete project " + this.props.module.value.name + "?"}<br/>
-                    This cannot be undone!.
+                    This cannot be undone!
                 </Modal.Body>
                 <Modal.Footer>
                     <Button bsStyle="primary" onClick={closeModal}>Cancel</Button>
