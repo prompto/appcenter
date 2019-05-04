c985b2dc4eb350bd7249b55d7f674c60d4ecad66
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-05-04 11:43:37 +0800

    remove unusable test

--- CodeFactory/EditorApp/src/web/src/EditorPage.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import EditorPage from './EditorPage';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<EditorPage />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
