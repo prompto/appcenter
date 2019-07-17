361e676647fab2f1cc03d26dcb246a5495a742f5
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2019-07-17 21:41:26 +0800

    in DEV mode, get js libs from server

--- CodeFactory/ProjectsApp/src/web/src/setupProxy.js
+++ CodeFactory/ProjectsApp/src/web/src/setupProxy.js
@@ -2,5 +2,6 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use("/ws", proxy('http://localhost:8000/ws'));
+    app.use("/js", proxy('http://localhost:8000/js'));
 };
 
