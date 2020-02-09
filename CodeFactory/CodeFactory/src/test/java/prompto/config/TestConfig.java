aca784a2197dabe0e65573cf8e8a676982b1b27e
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-02-09 11:14:02 +0800

    fix failing tests

--- CodeFactory/CodeFactory/src/test/java/prompto/config/TestConfig.java
+++ CodeFactory/CodeFactory/src/test/java/prompto/config/TestConfig.java
@@ -14,18 +14,18 @@ public class TestConfig {
 
 	@Test
 	public void targetIsRead() {
-		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("home-prompto-apps.yml");
+		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("configs/home-factory-dev-factory-apps-apps.yml");
 		IConfigurationReader reader = new YamlConfigurationReader(input);
 		ICodeFactoryConfiguration config = new CodeFactoryConfiguration(reader, Collections.emptyMap());
 		ITargetConfiguration target = config.getTargetConfiguration();
-		assertEquals("PROMPTO-DATA", target.getDataStoreConfiguration().getDbName());
+		assertEquals("APPS", target.getDataStoreConfiguration().getDbName());
 		assertEquals(8080, target.getPortRangeConfiguration().getMinPort());
 		assertEquals(9090, target.getPortRangeConfiguration().getMaxPort());
 	}
 	
 	@Test
 	public void fetchesLoginStore() throws Throwable {
-		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("test-login-store.yml");
+		InputStream input = Thread.currentThread().getContextClassLoader().getResourceAsStream("configs/test-login-store.yml");
 		IConfigurationReader reader = new YamlConfigurationReader(input);
 		ICodeFactoryConfiguration config = new CodeFactoryConfiguration(reader, Collections.emptyMap());
 		IStore store = Application.fetchLoginStore(config);
