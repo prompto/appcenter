9531a1dd32d7bb4a7f25e2f86355bd9f4d504754
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-02-23 10:29:26 +0800

    change dev proxy port

--- CodeFactory/EditorApp/src/web/src/setupProxy.js
+++ CodeFactory/EditorApp/src/web/src/setupProxy.js
@@ -1,7 +1,7 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use("/ws", proxy('http://localhost:8888/ws'));
-    app.use("/js/lib", proxy('http://localhost:8888/js/lib'));
+    app.use("/ws", proxy('http://localhost:8080/ws'));
+    app.use("/js/lib", proxy('http://localhost:8080/js/lib'));
 };
 
