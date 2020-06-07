0be180655cf87007f7672fb24dd8d52bcdd5dc59
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-06-08 02:19:35 +0800

    simplify login page

--- CodeFactory/CodeFactory/src/main/resources/auth/loginPage.jsx
+++ CodeFactory/CodeFactory/src/main/resources/auth/loginPage.jsx
@@ -2,47 +2,9 @@ const { Navbar, Nav, NavItem, Modal, FormGroup, ControlLabel, FormControl, Butto
 
 class LoginPage extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {name: null, password: null};
-        this.handleName = this.handleName.bind(this);
-        this.handlePassword = this.handlePassword.bind(this);
-        this.handleConnect = this.handleConnect.bind(this);
-    }
-
     handleClose() {
         document.location = "/auth/errorPage.html";
     }
-
-    handleConnect() {
-        axios({
-            method: 'post',
-            url: "/j_security_check",
-            headers: {
-                'Content-type': 'application/x-www-form-urlencoded'
-            },
-            params: {
-                'j_username': encodeURI(this.state.name),
-                'j_password': encodeURI(this.state.password)
-            }
-        }).then(response=>{
-            document.open("text/html", "replace");
-            document.write(response.data);
-            document.close();
-        }).catch(error=>{
-            console.log(error);
-            document.location = "/auth/errorPage.html";
-        });
-    }
-
-    handleName(e) {
-        this.setState( { name: e.currentTarget.value } );
-    }
-
-    handlePassword(e) {
-        this.setState( { password: e.currentTarget.value } );
-    }
-
 
     render() {
         const btnStyle = { backgroundImage: "none" };
@@ -63,22 +25,24 @@ class LoginPage extends React.Component {
                     <Modal.Header closeButton={true}>
                         <Modal.Title>Connect to your development server</Modal.Title>
                     </Modal.Header>
-                    <Modal.Body style={{padding: "8px"}}>
-                        <form style={{margin: "8px"}} method='post'>
-                            <FormGroup>
-                                <ControlLabel>Name</ControlLabel><br/>
-                                <FormControl type="text" id="name" onChange={this.handleName}/>
-                            </FormGroup>
-                            <FormGroup>
-                                <ControlLabel>Password</ControlLabel><br/>
-                                <FormControl type="password" id="description" onChange={this.handlePassword}/>
-                            </FormGroup>
-                        </form>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button style={btnStyle} onClick={this.handleClose}>Cancel</Button>
-                        <Button style={btnStyle} bsStyle="primary" onClick={this.handleConnect}>Connect</Button>
-                    </Modal.Footer>
+	                <form method='post' action="/j_security_check">
+	                    <Modal.Body style={{padding: "8px"}}>
+							<div style={{margin: "8px"}}>
+	                            <FormGroup>
+	                                <ControlLabel>Name</ControlLabel><br/>
+	                                <FormControl type="text" id="name"/>
+	                            </FormGroup>
+	                            <FormGroup>
+	                                <ControlLabel>Password</ControlLabel><br/>
+	                                <FormControl type="password" id="description"/>
+	                            </FormGroup>
+							</div>
+	                    </Modal.Body>
+	                    <Modal.Footer>
+	                        <Button style={btnStyle} onClick={this.handleClose}>Cancel</Button>
+	                        <Button style={btnStyle} bsStyle="primary" type="submit">Connect</Button>
+	                    </Modal.Footer>
+                    </form>
                 </Modal>
             </div>;
     }
