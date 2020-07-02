4c38a87890e6616f1bd3362812a2c58f03a15119
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2020-07-03 00:45:44 +0800

    Set default user for testing

--- CodeFactory/CodeFactory/src/test/java/prompto/codefactory/TestApplication.java
+++ CodeFactory/CodeFactory/src/test/java/prompto/codefactory/TestApplication.java
@@ -1,8 +1,11 @@
 package prompto.codefactory;
 
+import prompto.server.AppServer;
+
 public class TestApplication {
 
 	public static void main(String[] args) throws Throwable {
+		AppServer.setHttpUser("eric.vergnaud@wanadoo.fr");
 		Application.main(args);
 	}
 
