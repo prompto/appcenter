0c0d585829ce446e03cc128bacccbecf260df4f0
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2021-10-20 15:37:29 +0200

    improve naming

--- CodeFactory/CodeFactory/src/test/java/prompto/codefactory/TestModuleProcess.java
+++ CodeFactory/CodeFactory/src/test/java/prompto/codefactory/TestModuleProcess.java
@@ -7,10 +7,10 @@ import org.junit.Test;
 public class TestModuleProcess {
 
 	@Test
-	public void testLocateYamlConfigFile() {
+	public void locatesYamlConfigFile() {
 		String cmdLine = "prompto.devcenter.Application -yamlConfigFile /Users/ericvergnaud/Development/prompto/prompto-dev-center/DevCenter/src/test/resources/deploy-prompto-seed.yml";
 		String location = "/Users/ericvergnaud/Development/prompto/prompto-dev-center/DevCenter/src/test/resources/deploy-prompto-seed.yml";
 		assertEquals(location, ModuleProcess.extractCmdLineArgument(cmdLine, "-yamlConfigFile"));
 	}
-
+	
 } 
